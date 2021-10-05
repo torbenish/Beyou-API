870a7431101b7805d0f68f944830fd626cacf4c9
Ishtrb <ishiitorben@gmail.com>
2021-10-04 21:00:24 -0300

    :zap: Add category

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 const express = require("express");
-const { restart } = require("nodemon");
 const app = express();
 
-require('dotenv').config()
+require("dotenv").config();
 
-const cors = require('cors')
+const cors = require("cors");
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 const Post = require("./models/Posts");
 
 app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
 app.get("/hello_world", (req, res) => {
   res.send("hello world");
@@ -25,9 +24,9 @@ app.post("/create", (req, res) => {
 
 app.post("/create_post", async (req, res) => {
   try {
-    const { title, content } = req.body;
+    const { title, summary, content, category } = req.body;
 
-    const post = await Post.create({ title, content });
+    const post = await Post.create({ title, summary, content, category });
 
     res.send(post);
   } catch (err) {
@@ -35,54 +34,71 @@ app.post("/create_post", async (req, res) => {
   }
 });
 
-app.get('/list_posts', async (req,res) => {
-    try{
-        const posts = await Post.find()
+app.get("/list_posts", async (req, res) => {
+  try {
+    const posts = await Post.find();
 
-        res.send({ posts })
-    }catch(err) {
-        res.status(400).send(err)
-    }
-})
+    res.send({ posts });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
-app.get('/show_post/:post_id', async (req, res) => {
-    try {
+app.get("/show_post/:post_id", async (req, res) => {
+  try {
+    const postId = req.params.post_id;
 
-        const postId = req.params.post_id
+    const post = await Post.findById(postId);
 
-        const post = await Post.findById(postId)
+    res.send({ post });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
-        res.send({ post })
-    } catch(err) {
-        res.status(400).send(err)
-    }
-})
+app.get("/list_posts/1", async (req, res) => {
+  try { 
 
-app.patch('/update_post/:post_id', async (req, res) => {
-    try {
-        const postId = req.params.post_id
+    const saudeId = req.params.saudeId;
 
-        const { title, content } = req.body
+    const post = await Post.find({ category: 'saude'});
+ 
+    res.send({ post} );
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
-        const post = await Post.findByIdAndUpdate(postId, { title, content }, { new: true })
-        
-        res.send({ post })
-    } catch(err) {
-        res.status(400).send(err)
-    }
-})
 
-app.delete('/delete_post/:post_id', async (req, res) => {
-    try {
-        const postId = req.params.post_id
+app.patch("/update_post/:post_id", async (req, res) => {
+  try {
+    const postId = req.params.post_id;
 
-        await Post.findByIdAndDelete(postId)
+    const { title, summary, content, category } = req.body;
 
-        res.send({ msg: 'Deletado com sucesso' })
-    } catch(err) {
-        res.status(400).send(err)
-    }
-})
+    const post = await Post.findByIdAndUpdate(
+      postId,
+      { title, summary, content, category },
+      { new: true }
+    );
+
+    res.send({ post });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.delete("/delete_post/:post_id", async (req, res) => {
+  try {
+    const postId = req.params.post_id;
+
+    await Post.findByIdAndDelete(postId);
+
+    res.send({ msg: "Deletado com sucesso" });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
