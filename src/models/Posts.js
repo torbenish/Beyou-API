93337c00686a07ad065f7ad09e25377bf2d58ab7
Ishtrb <ishiitorben@gmail.com>
2021-10-04 21:30:23 -0300

    :zap: Add Posts

--- src/models/Posts.js
+++ src/models/Posts.js
@@ -1,20 +1,28 @@
-const mongoose = require('../database/db')
+const mongoose = require("../database/db");
 
 const PostSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    content: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  summary: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
-const Post = mongoose.model('Post', PostSchema)
+const Post = mongoose.model("Post", PostSchema);
 
-module.exports = Post
+module.exports = Post;
